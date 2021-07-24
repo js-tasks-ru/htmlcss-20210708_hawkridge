d80c948c05c2d09bf26f40b7dfa8d46beadeae4d
Берилло Дмитрий Юрьевич <dmitry.beryllo@privatbank.ua>
2021-07-24 22:40:08 +0300

    feat(global): add modal

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -2,12 +2,14 @@
     const close = document.getElementById('modal-close');
     const modal = document.getElementById('modal');
     const modalOpen = document.getElementById('modal-open');
+    console.log('modalOpen', modalOpen);
 
     if (!close || !modalOpen || !modal) {
         return;
     }
 
     modalOpen.addEventListener('click', () => {
+        console.log('click');
         modal.style.display = 'block';
         document.body.overflow = 'hidden';
     });
